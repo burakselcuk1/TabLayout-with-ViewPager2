0059d08f773dbedbfc3d1cb79079a796f17fa5c6
burak <brkdewils@gmail.com>
2022-02-13 04:11:57 +0300

    TabLayout with ViewPager2

---
+++ app/src/main/java/com/example/test6/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.test6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val adapter = ViewPagerAdapter(supportFragmentManager,lifecycle)
+        view_pager.adapter = adapter
+
+        TabLayoutMediator(tabLayout,view_pager){tab,position->
+            when(position){
+                0->{
+                    tab.text ="First"
+                }
+                1->{
+                    tab.text="Second"
+                }
+                2->{
+                    tab.text="Third"
+                }
+                3->{
+                    tab.text="Fourth"
+                }
+                4->{
+                    tab.text="Fiveth"
+                }
+            }
+        }.attach()
+    }
+}
---
+++ app/src/main/java/com/example/test6/ViewPagerAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.test6
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle): FragmentStateAdapter(fragmentManager,lifecycle) {
+    override fun getItemCount(): Int {
+        return 5
+    }
+
+    override fun createFragment(position: Int): Fragment {
+       return when(position){
+            0->{
+                FirstFragment()
+            }
+            1->{
+                SecondFragment()
+            }
+            2->{
+                ThirdFragment()
+            }
+            3->{
+                FourthFragment()
+            }
+            4->{
+                FivethFragment()
+            }
+            else->{
+                Fragment()
+            }
+        }
+    }
+}
